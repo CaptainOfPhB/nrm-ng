7a1563c1fd6927f09f75762a2526714b9ab40548
CaptainOfPhB <captainofphb@gmail.com>
2022-11-10 10:23:16 +0800

    chore(index.ts): use commander

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
-import fetch from 'node-fetch'
+import { Command } from 'commander';
+import packageJson from '../package.json';
 
-fetch("https://nrs-get-latest-registries.deno.dev/")
-  .then(res => res.json())
-  .then(json => console.log(json));
+import { exampleUsage } from './helper';
+import { onAdd, onDelete, onList, onTest, onUse } from './actions';
+
+const program = new Command();
+
+program
+  .name(packageJson.name)
+  .version(packageJson.version)
+  .description(packageJson.description);
+
+program
+  .command('ls')
+  .description('List all registries')
+  .action(onList)
+  .addHelpText('after', exampleUsage(['$ nrm ls']));
+
+program
+  .command('use <name>')
+  .description('Switch registry')
+  .action(onUse)
+  .addHelpText('after', exampleUsage(['$ nrm use gh']));
+
+program
+  .command('add <name> <registry>')
+  .description('Add a new registry')
+  .action(onAdd)
+  .addHelpText('after', exampleUsage(['$ nrm add gh https://npm.pkg.github.com']));
+
+program
+  .command('rm <name>')
+  .description('Delete a registry')
+  .action(onDelete)
+  .addHelpText('after', exampleUsage(['$ nrm rm gh']));
+
+program
+  .command('test')
+  .description('Test the response speed of all registries')
+  .action(onTest)
+  .addHelpText('after', exampleUsage(['$ nrm test']));
+
+program.parse();
