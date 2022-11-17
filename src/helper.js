aff256b1688d45797217c419e8cffbcc27943a02
CaptainOfPhB <captainofphb@gmail.com>
2022-11-18 00:07:14 +0800

    chore(helper.js): update

--- src/helper.js
+++ src/helper.js
@@ -1,24 +1,35 @@
 const { command } = require('execa');
+const { bgGreen, green, bgRed, red, black } = require('picocolors');
 
 function exampleUsage(commands) {
   const usages = commands.map(command => `  ${command}`).join('`n');
   return '\nExample usage:\n' + usages;
 }
 
+function printSuccess(message) {
+  console.log(`${bgGreen(black(' SUCCESS '))} ${green(message)}`);
+}
+
+function printError(error) {
+  console.log(`${bgRed(black(' ERROR '))} ${red(error)}`);
+}
+
 function run(cmd, options) {
   return command(cmd, options)
     .then(result => result.stdout)
     .catch(error => {
-      console.error(error.shortMessage);
+      printError(error.shortMessage);
       return null;
     });
 }
 
 function getCurrentRegistry() {
-  return run('npm config gt registry');
+  return run('npm config get registry');
 }
 
 module.exports = {
   exampleUsage,
+  printSuccess,
+  printError,
   getCurrentRegistry,
 };
