c28024785352f9a9324dd1bfdeb3ec161b5928c2
CaptainOfPhB <captainofphb@gmail.com>
2023-06-23 16:08:01 +0800

    chore: rename filename

--- registry-service.ts
+++ registry-service.ts
@@ -3,7 +3,7 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 async function handleRequest() {
-  const registries = await Deno.readFile("./registries.json");
+  const registries = await Deno.readFile("./registry.json");
   return new Response(registries, {
     headers: {
       "content-type": "application/json",
